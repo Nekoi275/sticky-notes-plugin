1306a36c0f188d2cbc4affbb076cbfddbcaef4da
nekoi <nekoi275@gmail.com>
2019-07-01 18:49:00 +0300

    changed setting note handlers

--- js/main.js
+++ js/main.js
@@ -3,17 +3,21 @@ var noteId = 0;
 var maxZIndex = 1;
 var movingNote;
 
-function createNote() {
-    noteId++;
+function createNote(noteStatus) {
     var note = document.createElement('div');
     note.classList.add('note');
-    note.setAttribute('data-note-id', noteId);
+    note.setAttribute('data-note-id', noteStatus.id);
     note.innerHTML = '<div class="note-move-button"></div>' +
                      '<div class="note-remove-button"><span>&#128465;</span></div>' +
                      '<div class="note-textarea">' +
                        '<textarea></textarea>' +
                      '</div>';
     document.body.appendChild(note);
+    setNoteHandlers(note);
+    return note;
+};
+
+function setNoteHandlers(note) {
     note.addEventListener('mousedown', function(event) {
         if (event.target.className === 'note-move-button') {
             movingNote = note;
@@ -22,14 +26,20 @@ function createNote() {
             note.style.zIndex = maxZIndex++;
         };
     });
+
+    var removeButton = note.getElementsByClassName('note-remove-button')[0]
+    removeButton.addEventListener('click', function(event) {
+        document.body.removeChild(note);
+        var noteId = note.getAttribute('data-note-id');
+        localStorageRemoveNote(noteId);
+        event.stopPropagation();
+    });
+
     var textArea = note.getElementsByTagName('textarea')[0]
     textArea.addEventListener('blur', function(){
         var noteStatus = getNoteStatus(note);
         localStorageSaveNote(noteStatus);
     });
-    addRemoveNoteHandler(note);
-    var noteStatus = getNoteStatus(note);
-    localStorageSaveNote(noteStatus);
 };
 
 function getNoteStatus(noteElem) {
@@ -76,16 +86,6 @@ function localStorageGetNotes() {
     };
 };
 
-function addRemoveNoteHandler(note) {
-    var removeButton = note.getElementsByClassName('note-remove-button')[0]
-    removeButton.addEventListener('click', function(event) {
-        document.body.removeChild(note);
-        var noteId = note.getAttribute('data-note-id');
-        localStorageRemoveNote(noteId);
-        event.stopPropagation();
-    });
-};
-
 document.addEventListener('mousemove', function(event) {
     if (movingNote) {
         document.body.style.overflow = 'hidden';
@@ -113,4 +113,8 @@ document.addEventListener('mouseup', function() {
     };
 });
 
-addButton.addEventListener('click', createNote);
+addButton.addEventListener('click', function() {
+    var note = createNote({id: noteId++});
+    var noteStatus = getNoteStatus(note);
+    localStorageSaveNote(noteStatus);
+});
