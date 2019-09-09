ffbe658bf185faab48b7c8e4b95a20af04375d7e
nekoi <nekoi275@gmail.com>
2019-09-09 12:06:45 +0300

    added parameters to readme file

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var addButton = document.getElementsByClassName('note-add')[0]
 var workspaceParams = {width: 1200, height: 500, parent: document.body};
 var notes = new StickyNotes(addButton, workspaceParams);
-//notes.createNote({X: 100, Y: 0, text: 'lorem ipsum', width: -500, height: 700});
+//notes.createNote({X: 100, Y: 0, text: 'lorem ipsum', width: 500, height: 700});
 //var localNotes = notes.localStorageGetNotes();
 //console.log(localNotes);
